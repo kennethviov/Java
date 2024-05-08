748ca9640dc6ccf9e01217dc2bf46fcd88fec7f5
kennethviov <151752526+kennethviov@users.noreply.github.com>
2024-05-09 05:44:50 +0800

    PasswordChecker.java

--- Chapter44/PasswordChecker.java
+++ Chapter44/PasswordChecker.java
@@ -1,12 +1,47 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class PasswordChecker {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Enter your password: ");
-        String pswrd = scan.nextLine();
+        //input
+        System.out.print("Enter password: ");
+        String password = scan.nextLine();
 
-        
+        //output
+        if (password.length() > 7) {
+            if (CheckCase(password)) {
+                if (CheckDigits(password))
+                    System.out.println("Password is valid.");
+                else
+                    System.out.println("Password must contain numbers.");
+            } else
+                System.out.println("Password must contain both uppercase and lowercase letters.");
+        } else
+            System.out.println("Password must be at least 7 characters long.");
+
+        scan.close();
+    }
+
+    static boolean CheckCase (String password) {
+        boolean upper = false;
+        boolean lower = false;
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isUpperCase(password.charAt(i)))
+                upper = true;
+            else if (Character.isLowerCase(password.charAt(i)))
+                lower = true;
+        }
+        if (upper && lower)
+            return true;
+        else
+            return false;
+    }
+
+    static boolean CheckDigits (String password) {
+        if (password.contains("1") || password.contains("2") || password.contains("3") || password.contains("4") || password.contains("5") || password.contains("6") || password.contains("7") || password.contains("8") || password.contains("9") || password.contains("0"))
+            return true;
+        else 
+            return false;
     }
 }
