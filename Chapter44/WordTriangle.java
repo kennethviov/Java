99817a34c1b2eb9c308bdfce1c417378433e533f
kennethviov <151752526+kennethviov@users.noreply.github.com>
2024-05-09 05:45:31 +0800

    WordTriangle.java

--- Chapter44/WordTriangle.java
+++ Chapter44/WordTriangle.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 
 public class WordTriangle {
-    public static void main (String[] args) {
-        Scanner scan = new Scanner (System.in);
-
-        System.out.print("Enter a word -->  ");
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a word --> ");
         String word = scan.nextLine();
 
-        for (int i = 0; i <= word.length(); i++) {
-            
+        for (int i = 0; i < word.length() / 2 + 1; i++) {
+            String spaces = new String(new char[i]).replace('\0', ' ');
+            String modifiedWord = spaces + word.substring(i, word.length() - i);
+            System.out.println(modifiedWord);
         }
+
+        scan.close();
     }
 }
